6417d5bab58df14aefdeb901033a6754ddc07471
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-10-17 12:00:44 -0300

    Alterações no css

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,33 +1,38 @@
 let background = document.querySelector('.background__container');
 let menu_button = document.querySelector('.color__button');
-let boxshadow_header = document.querySelector('.box-shadow');
+let border_header = document.getElementsByTagName('header');
 let pageName = document.querySelector('.color__titulo');
 let text_title = document.getElementsByTagName('h2');
-let boxshadow_footer = document.getElementsByTagName('footer');
+let menu_color = document.querySelector('.menu__lista');
+let border_footer = document.getElementsByTagName('footer');
 
-let colorChose = Math.floor(Math.random() * 2);
+let colorChose = 1;
 
-if(colorChose === 0) colorSet('acid')
-if(colorChose === 1) colorSet('synthwave')
+let win = window.innerWidth;
 
-console.log(colorChose)
-
+console.log(menu_button)
+if(colorChose === 0) colorSet('acid');
+if(colorChose === 1) colorSet('synthwave');
 
 function colorSet(themeName) {
     for(let i = 0; i < text_title.length; i++){
-        text_title[i].classList.remove('color__titulo')
-        text_title[i].classList.add(`color__titulo-${themeName}-2`)
+        text_title[i].classList.remove('color__titulo');
+        text_title[i].classList.add(`color__titulo--${themeName}-2`);
     }
+
+    if(win < 800) menu_color.classList.add(`menu__lista--${themeName}`);
+
+    background.classList.remove('background__container');
+    menu_button.classList.remove('color__button');
+    border_header[0].classList.remove('box-shadow');
+    pageName.classList.remove('color__titulo');
+    border_footer[0].classList.remove('box-shadow');
+
+    background.classList.add(`background__container--${themeName}`);
+    menu_button.classList.add(`color__button--${themeName}`);
+    border_header[0].classList.add(`border__header--${themeName}`);
+    pageName.classList.add(`color__titulo--${themeName}`);
+    border_footer[0].classList.add(`border__footer--${themeName}`);
     
-    background.classList.remove('background__container')
-    menu_button.classList.remove('color__button')
-    boxshadow_header.classList.remove('box-shadow')
-    pageName.classList.remove('color__titulo')
-    boxshadow_footer[0].classList.remove('box-shadow')
-    
-    background.classList.add(`background__container-${themeName}`)
-    menu_button.classList.add(`color__button-${themeName}`)
-    boxshadow_header.classList.add(`box-shadow-${themeName}`)
-    pageName.classList.add(`color__titulo-${themeName}`)
-    boxshadow_footer[0].classList.add(`box-shadow-${themeName}`)
+    return
 }
